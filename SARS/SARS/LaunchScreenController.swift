9f25fd0a25907b15d1ced5995898c1325d44a71c
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-03-12 18:37:54 -0700

    Lauch Setup

---
+++ SARS/SARS/LaunchScreenController.swift
@@ -0,0 +1,15 @@
+//
+//  LaunchScreenController.swift
+//  SARS
+//
+//  Created by Nikil Roashan Selvam on 3/12/19.
+//  Copyright © 2019 Neil Sonnenberg. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+class LaunchScreenController : UIViewController {
+}
