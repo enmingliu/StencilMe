99be8da2b252e3902cea1a4083fe7eb28a2e22f6
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-03-12 18:40:03 -0700

    Added assets

--- SARS/SARS/LaunchScreenController.swift
+++ SARS/SARS/LaunchScreenController.swift
@@ -12,4 +12,44 @@ import UIKit
 
 
 class LaunchScreenController : UIViewController {
+    
+    @IBOutlet weak var pencil : UIImageView!
+    @IBOutlet weak var spray: UIImageView!
+    @IBOutlet weak var logo: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        self.pencil.alpha=0
+        self.spray.alpha=0
+        self.logo.alpha=0
+        super.viewDidLoad();
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.pencil.alpha=0
+        self.spray.alpha=0
+        self.logo.alpha=0
+        self.pencil.center.x-=100;
+        self.spray.center.x+=100;
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIView.animate(withDuration: 2.0, animations: {
+            self.pencil.transform = CGAffineTransform(translationX: 100, y: 0)
+            self.spray.transform = CGAffineTransform(translationX: -100, y: 0)
+            
+            self.pencil.alpha=1
+            self.spray.alpha=1
+            self.logo.alpha=1
+            
+        })
+        {(success) in
+            
+            usleep(2000000)
+            self.performSegue(withIdentifier: "segueMainScreen", sender: self)
+        }
+        
+    }
+    
 }
